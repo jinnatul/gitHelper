efab120fe9fb1c29ba60353e28e26e507b4edac4
jinnatul <morolswediu@gmail.com>
2020-06-02 16:04:14 +0600

    formatting structure

---
+++ assets/js/git_forkingWorkflow.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  let gitInfo = "<ul class='collapsible expandable'>"
+
+  // 
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ </h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // 
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ </h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // 
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ </h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // 
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ </h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // 
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ </h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  $('#git_forkingWorkflow').html(gitInfo);
+
+})
