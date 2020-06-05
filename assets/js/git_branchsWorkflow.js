afd63fde7f5e09282c82198ec61710a6e538c667
jinnatul <morolswediu@gmail.com>
2020-06-05 09:23:57 +0600

    Feature Branch Workflow

--- assets/js/git_branchsWorkflow.js
+++ assets/js/git_branchsWorkflow.js
@@ -2,30 +2,45 @@ $(function() {
 
   let gitInfo = "<ul class='collapsible expandable'>"
 
-  // 
+  // Concept of Local Storage & Remote Storage
   + "<li><div class='collapsible-header'>"
-  + "<h6>$ </h6></div>"
-  + "<div class='collapsible-body codejs'><p class='fontSize'></p></div></li>"
+  + "<h6>$ Concept of Local Storage & Remote Storage</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
 
-  // 
+  // Create a local repository with git clone
   + "<li><div class='collapsible-header'>"
-  + "<h6>$ </h6></div>"
-  + "<div class='collapsible-body codejs'><p class='fontSize'></p></div></li>"
+  + "<h6>$ Create a local repository with git clone</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
 
-  // 
+  // Check branch location
   + "<li><div class='collapsible-header'>"
-  + "<h6>$ </h6></div>"
-  + "<div class='collapsible-body codejs'><p class='fontSize'></p></div></li>"
+  + "<h6>$ Check branch location</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
 
-  // 
+  // Create a new branch
   + "<li><div class='collapsible-header'>"
-  + "<h6>$ </h6></div>"
-  + "<div class='collapsible-body codejs'><p class='fontSize'></p></div></li>"
+  + "<h6>$ Create a new branch</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
 
-  // 
+  // Push the branch to the central remote repository
   + "<li><div class='collapsible-header'>"
-  + "<h6>$ </h6></div>"
-  + "<div class='collapsible-body codejs'><p class='fontSize'></p></div></li>"
+  + "<h6>$ Push the branch to the central remote repository</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // Send a full request to the project manager
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ Send a full request to the project manager</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // Synchronization
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ Synchronization</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
+
+  // Create another branch for the new function
+  + "<li><div class='collapsible-header'>"
+  + "<h6>$ Create another branch for the new function</h6></div>"
+  + "<div class='collapsible-body codejs'></div></li>"
 
   $('#git_branchsWorkflow').html(gitInfo);
 
