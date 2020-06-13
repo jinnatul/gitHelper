9b5a5e25c4f63ccdb298ade3a1971ada4c21eacc
jinnatul <morolswediu@gmail.com>
2020-06-13 20:33:53 +0600

    add ইন্সটল (Setup)

--- assets/js/git_bangla.js
+++ assets/js/git_bangla.js
@@ -2,10 +2,21 @@ $(function() {
 
   $.getJSON('assets/json/bangla.json', function() {})
     .done(function(res) {
-      console.log(res.gitData[0]);
+      //console.log(res.gitData);
+      ইনসটল(res.gitData[0]);
     })
     .fail(function() {
       console.log('Problem ....here!');
     })
 
 });
+
+function ইনসটল(data) {
+  console.log(data)
+  let tableData = "<table><tr><th>"+ data.title +"</th></tr>"; 
+  for (let index = 0; index < data.items.length; index++) {
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+  }
+  tableData += "</table>";
+  $('#gitInstall').html(tableData);
+}
