a5560a26efe8682324cd99eaff1bb74c23d011be
jinnatul <morolswediu@gmail.com>
2020-06-13 20:46:54 +0600

    add সাধারণ (General)

--- assets/js/git_bangla.js
+++ assets/js/git_bangla.js
@@ -4,6 +4,7 @@ $(function() {
     .done(function(res) {
       ইনসটল(res.gitData[0]);
       কনফিগ(res.gitData[1]);
+      সাধারণ(res.gitData[2]);
     })
     .fail(function() {
       console.log('Problem ....here!');
@@ -12,7 +13,7 @@ $(function() {
 });
 
 function ইনসটল(data) {
-  let tableData = "<table><tr><th>"+ data.title +"</th></tr>"; 
+  let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>"; 
   for (let index = 0; index < data.items.length; index++) {
     tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
   }
@@ -21,10 +22,19 @@ function ইনসটল(data) {
 }
 
 function কনফিগ(data) {
-  let tableData = "<table><tr><th>"+ data.title +"</th></tr>"; 
+  let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
     tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
   }
   tableData += "</table>";
   $('#gitConfig').html(tableData);
+}
+
+function সাধারণ(data) {
+  let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
+  for (let index = 0; index < data.items.length; index++) {
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+  }
+  tableData += "</table>";
+  $('#gitGeneral').html(tableData);
 }
