21b4c5e7524928d7a06acf0b247e5b6f84b2797f
jinnatul <morolswediu@gmail.com>
2020-06-13 20:37:19 +0600

    add কনফিগ (Config)

--- assets/js/git_bangla.js
+++ assets/js/git_bangla.js
@@ -2,21 +2,29 @@ $(function() {
 
   $.getJSON('assets/json/bangla.json', function() {})
     .done(function(res) {
-      //console.log(res.gitData);
       ইনসটল(res.gitData[0]);
+      কনফিগ(res.gitData[1]);
     })
     .fail(function() {
       console.log('Problem ....here!');
-    })
+    });
 
 });
 
 function ইনসটল(data) {
-  console.log(data)
   let tableData = "<table><tr><th>"+ data.title +"</th></tr>"; 
   for (let index = 0; index < data.items.length; index++) {
     tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
   }
   tableData += "</table>";
   $('#gitInstall').html(tableData);
+}
+
+function কনফিগ(data) {
+  let tableData = "<table><tr><th>"+ data.title +"</th></tr>"; 
+  for (let index = 0; index < data.items.length; index++) {
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+  }
+  tableData += "</table>";
+  $('#gitConfig').html(tableData);
 }
