ca459eddf3ed2ceffaaa90cb7e4843c14934e19b
jinnatul <morolswediu@gmail.com>
2020-06-15 21:45:24 +0600

    add clipBoard on Bangla-git

--- assets/js/git_bangla.js
+++ assets/js/git_bangla.js
@@ -17,17 +17,46 @@ $(function() {
       গিট_ফলো(res.gitData[12]);
       গিটহাব_ইসু(res.gitData[13]);
 
+      // Copy clipBoard
+      copyData_ClipBoard();
     })
     .fail(function() {
       console.log('Problem ....here!');
     });
 
+    
+
 });
+
+function copyData_ClipBoard() {
+  $(".content").on("click",function(){
+    let currentId = 'ID' + this.id; 
+    let copyText = $('#' + currentId).text();
+    let input = document.createElement('input');
+    input.value = copyText;
+    
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    showMaterialToast("Copied ", "deep-purple darken-4");
+    document.body.removeChild(input);
+ });
+}
+
+function showMaterialToast(data, style) {
+  M.toast({
+      html : data,
+      classes : style
+  });
+}
 
 function ইনসটল(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>"; 
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDinstall-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='install-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitInstall').html(tableData);
@@ -36,7 +65,10 @@ function ইনসটল(data) {
 function কনফিগ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDconfig-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='config-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitConfig').html(tableData);
@@ -45,7 +77,10 @@ function কনফিগ(data) {
 function সাধারণ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDnormal-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='normal-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitGeneral').html(tableData);
@@ -54,7 +89,10 @@ function সাধারণ(data) {
 function রিসেট(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDreset-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='reset-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitReset').html(tableData);
@@ -63,7 +101,10 @@ function রিসেট(data) {
 function আপডেট_এবং_ডিলিট(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDupdate-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='update-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitUpdateDelete').html(tableData);
@@ -72,7 +113,10 @@ function আপডেট_এবং_ডিলিট(data) {
 function বাঞচ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDbranch-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='branch-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitBranch').html(tableData);
@@ -81,7 +125,10 @@ function বাঞচ(data) {
 function মারজ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDmarge-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='marge-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitMerge').html(tableData);
@@ -90,7 +137,10 @@ function মারজ(data) {
 function সটাশ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDstash-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='stash-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitStash').html(tableData);
@@ -99,7 +149,10 @@ function সটাশ(data) {
 function লগ(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDlog-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='log-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitLog').html(tableData);
@@ -108,7 +161,10 @@ function লগ(data) {
 function তুলনা(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDcompare-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='compare-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitCompare').html(tableData);
@@ -117,7 +173,10 @@ function তুলনা(data) {
 function রিলিজ_এবং_ভারসন(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDrelease-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='release-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitReleasesVersion').html(tableData);
@@ -126,7 +185,10 @@ function রিলিজ_এবং_ভারসন(data) {
 function সহযোগিতা(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDhelp-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='help-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitCollaborate').html(tableData);
@@ -135,7 +197,10 @@ function সহযোগিতা(data) {
 function গিট_ফলো(data) {
   let tableData = "<table><tr><h5 class='center-align blue-text'>"+ data.title +"</h5></tr>";
   for (let index = 0; index < data.items.length; index++) {
-    tableData += "<tr><td>"+ data.items[index].gitQus +"</td><td>"+ data.items[index].gitAns +"</td></tr>";
+    tableData += "<tr><td>"+ data.items[index].gitQus +"</td>"
+    tableData += "<td id='IDflow-"+ index +"'>"+ data.items[index].gitAns + "</td><td>"
+    tableData += "<a id='flow-"+ index +"' class='content'>"
+    tableData += "<i class='material-icons teal-text text-darken-1'>file_copy</i>copy</a></td></tr>";
   }
   tableData += "</table>";
   $('#gitFlow').html(tableData);
