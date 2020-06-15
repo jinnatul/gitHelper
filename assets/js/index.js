aad94d9aac4e76fe9fa8e4d7f13da6d3a987f597
zubayerhimel <zhimel007@gmail.com>
2020-06-15 17:22:17 +0600

    fixed responsiblity

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,29 @@
-$(function() {
+$(function () {
+  // preloader
+  $("main").hide();
+  setTimeout(function () {
+    $(".mainLoader").fadeOut();
+    $("main").show();
+  }, 1000);
 
-    // preloader
-    $("main").hide();
-    setTimeout(function() {
-        $('.mainLoader').fadeOut();
-        $("main").show();
-    }, 1000);
-    
+  // collapsible
+  $(document).ready(function () {
+    $(".collapsible").collapsible();
+  });
 
-    // collapsible
-    $(document).ready(function() {
-        $('.collapsible').collapsible();
-    })
+  // navbar dropdown
+  $(".dropdown-trigger").dropdown();
+  $(".sidenav").sidenav();
 
-    // navbar dropdown
-    $(".dropdown-trigger").dropdown();
-
-    // Smooth scrolling
-    var scrollLink = $(".scroll");
-    scrollLink.click(function(e) {
-        e.preventDefault();
-        $("body,html").animate(
-        {
-            scrollTop: $(this.hash).offset().top
-        },
-        2000
-        );
-    });
-})
+  // Smooth scrolling
+  var scrollLink = $(".scroll");
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      2000
+    );
+  });
+});
